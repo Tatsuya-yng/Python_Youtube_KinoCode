359cf65df9f39a043516554eda023df3638b7b1e
”tanakatatsuya” <tanaka@gmail.com>
2020-07-02 14:19:04 +0900

    リスト

---
+++ Lesson7.py
@@ -0,0 +1,7 @@
+a = ["sato","tanaka","suzuki"]
+
+a [1] = "yamada"
+
+print(a[0])
+print(a[1])
+print(a[2])
---
+++ Lesson8.py
@@ -0,0 +1,7 @@
+a = [["sato","tanaka"],["takahashi","suzuki"]]
+
+
+print(a[0][0])
+print(a[0][1])
+print(a[1][0])
+print(a[1][1])
