24d1de9cff5d24420cea5f5c29b7b89d125ce77a
”tanakatatsuya” <tanaka@gmail.com>
2020-07-04 23:45:41 +0900

    関数

---
+++ Lesson25.py
@@ -0,0 +1,6 @@
+def say_hello():
+    print("helloo world")
+
+say_hello()
+say_hello()
+say_hello()
---
+++ Lesson30.py
@@ -0,0 +1,9 @@
+# -*- coding: UTF-8
+# 確認問題　9と4と2の平均をだす
+
+
+def div(a,b,c):
+    return(a+b+c)/3
+
+div_result = div(9,4,2)
+print(div_result)
