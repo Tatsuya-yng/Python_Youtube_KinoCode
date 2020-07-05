921a21e998772132fa123b1b0f407e6d328c3059
”tanakatatsuya” <tanaka@gmail.com>
2020-07-05 15:49:36 +0900

    内容確認

--- Lesson20.py
+++ Lesson20.py
@@ -1,3 +1,6 @@
+# -*- coding: UTF-8
+#break文は、ある条件にあてはまったとき、繰り返し処理を終了させることができる
+
 for i in range(5):
     if i == 3:
         break
--- Lesson21.py
+++ Lesson21.py
@@ -1,3 +1,6 @@
+# -*- coding: UTF-8
+#continue文は、繰り返し処理で、ある条件にあてはまったときにその処理をスキップしたい場合
+
 for i in range(5):
     if i == 3:
         continue
